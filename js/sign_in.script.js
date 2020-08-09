18a5eeec0aee5d0d17e8d961c53b18af0a549120
eng-gabrielscardoso <67348001+eng-gabrielscardoso@users.noreply.github.com>
2020-08-09 11:15:02 -0300

    Update sign_in.script.js

--- js/sign_in.script.js
+++ js/sign_in.script.js
@@ -1,1 +1,22 @@
 /* sign in page script */
+$(function(){
+    $('#btnLoginPerson').click(function(){
+        $('#firstStepLogin').fadeOut();
+        $('#loginPerson').fadeIn();
+    });
+
+    $('#btnLoginVoluntary').click(function(){
+        $('#firstStepLogin').fadeOut();
+        $('#secondStepLoginVoluntary').fadeIn(function(){
+            $('#btnLoginVoluntaryPf').click(function(){
+                $('#secondStepLoginVoluntary').fadeOut();
+                $('#loginVoluntaryPf').fadeIn();;
+            });
+
+            $('#btnLoginVoluntaryPj').click(function(){
+                $('#secondStepLoginVoluntary').fadeOut();
+                $('#loginVoluntaryPj').fadeIn();
+            })
+        });
+    });
+});
